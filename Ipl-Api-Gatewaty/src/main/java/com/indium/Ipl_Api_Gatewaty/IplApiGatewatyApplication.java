870f656ada075ef2f2c16d1ce2f90f3e64b4bf6b
Nagashrith Janamanchi <ashrith6974@gmail.com>
2024-09-15 22:44:19 +0530

    Initial commit

---
+++ Ipl-Api-Gatewaty/src/main/java/com/indium/Ipl_Api_Gatewaty/IplApiGatewatyApplication.java
@@ -0,0 +1,13 @@
+package com.indium.Ipl_Api_Gatewaty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplApiGatewatyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplApiGatewatyApplication.class, args);
+	}
+
+}
---
+++ Ipl-Api-Gatewaty/src/test/java/com/indium/Ipl_Api_Gatewaty/IplApiGatewatyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indium.Ipl_Api_Gatewaty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplApiGatewatyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
